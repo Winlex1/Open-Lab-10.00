f4ea8131058693f68809941f3058bfae533ca4c8
Winlex__ <biker@MSI>
2022-11-30 20:02:30 +0100

    changes in program.cs

--- Open-Lab-10.00/Book.cs
+++ Open-Lab-10.00/Book.cs
@@ -1,18 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using System.Xml.Schema;
 
-namespace Open_Lab_10._00
+namespace Open_Lab_10._03
 {
     internal class Book
     {
-        public string title;
-        private int pages = 100;
+        private string title;
+        private int pages;
+        private string category;
+        private string author;
+        private string releaseDate;
+        public string Title { get { return title; } set { title = value; } }
+        public int Pages { get { return pages;} set {
+                if (value < 0)
+                {
+                    this.pages = 1;
+                }
+                else
+                {
+                    this.pages = value;
+                }
+            } }
+
+        public string Category { get { return category; } set { category = value; } }
+        public string Author { get { return author; } set { author = value; } }
+        public string ReleaseDate { get { return releaseDate; } 
+            set {
+                int n = Convert.ToInt32(value);
+                if(n < 1450 || n > 2021)
+                {
+                    this.releaseDate = "-1";
+                    
+                }
+                else
+                {
+                    this.releaseDate = value;
+                }
+                
+                
+            } }
+
+
+
         
-        public Book() { }
-        
+
     }
+
 }
--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -5,20 +5,32 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Open_Lab_10._01
+namespace Open_Lab_10._03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Appslab");
-            Book LOTR = new Book();
-            LOTR.title = "LOTR";
             
-            Console.WriteLine(LOTR.title);
+            Book LOTR = new Book();
+
+            LOTR.Author = "Winlex__";
+            LOTR.ReleaseDate = "2000";
+            LOTR.Title = "LOTR";
+            LOTR.Pages = 100;
+            LOTR.Category = "Fantasy";
+
+            Console.WriteLine(LOTR.Author);
+            Console.WriteLine(LOTR.ReleaseDate);
+            Console.WriteLine(LOTR.Title);
+            Console.WriteLine(LOTR.Pages);
+            Console.WriteLine(LOTR.Category);
+
+            
             
                 
 
         }
+        
     }
 }
